488d3731222d0fde4f346c723839a2d6251f069b
Valentin Kulichenko <valentin.kulichenko@gmail.com>
2020-07-18 13:35:51 -0700

    (get_or_)create_cache_with_configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -153,51 +153,54 @@ pub struct CacheConfiguration {
     pub query_entities: Vec<QueryEntity>,
 }
 
+macro_rules! write_property {
+    ($bytes:expr, $count:expr, $code:expr, $prop:expr) => {
+        $bytes.put_i16($code);
+        $prop.write($bytes)?;
+        $count = $count + 1;
+    };
+}
+
 impl IgniteWrite for CacheConfiguration {
     fn write(&self, bytes: &mut BytesMut) -> Result<()> {
-        let config_bytes = BytesMut::with_capacity(1024);
+        let mut config_bytes = BytesMut::with_capacity(1024);
+        let mut count = 0i16;
 
-        write_property(bytes, 2, &self.atomicity_mode)?;
-        write_property(bytes, 3, &self.backups)?;
-        write_property(bytes, 1, &self.mode)?;
-        write_property(bytes, 5, &self.copy_on_read)?;
-        write_property(bytes, 100, &self.data_region_name)?;
-        write_property(bytes, 405, &self.eager_ttl)?;
-        write_property(bytes, 406, &self.statistics_enabled)?;
-        write_property(bytes, 400, &self.group_name)?;
-        write_property(bytes, 402, &self.default_lock_timeout)?;
-        write_property(bytes, 403, &self.max_concurrent_async_operations)?;
-        write_property(bytes, 206, &self.max_query_iterators)?;
-        write_property(bytes, 0, &self.name)?;
-        write_property(bytes, 101, &self.on_heap_cache_enabled)?;
-        write_property(bytes, 404, &self.partition_loss_policy)?;
-        write_property(bytes, 202, &self.query_detail_metrics_size)?;
-        write_property(bytes, 201, &self.query_parallelism)?;
-        write_property(bytes, 6, &self.read_from_backup)?;
-        write_property(bytes, 303, &self.rebalance_batch_size)?;
-        write_property(bytes, 304, &self.rebalance_batch_prefetch_count)?;
-        write_property(bytes, 301, &self.rebalance_delay)?;
-        write_property(bytes, 300, &self.rebalance_mode)?;
-        write_property(bytes, 305, &self.rebalance_order)?;
-        write_property(bytes, 306, &self.rebalance_throttle)?;
-        write_property(bytes, 302, &self.rebalance_timeout)?;
-        write_property(bytes, 205, &self.sql_escape_all)?;
-        write_property(bytes, 204, &self.sql_index_inline_max_size)?;
-        write_property(bytes, 203, &self.sql_schema)?;
-        write_property(bytes, 4, &self.write_synchronization_mode)?;
-        write_property(bytes, 401, &self.cache_key_configurations)?;
-        write_property(bytes, 200, &self.query_entities)?;
+        write_property!(&mut config_bytes, count, 2, self.atomicity_mode);
+        write_property!(&mut config_bytes, count, 3, self.backups);
+        write_property!(&mut config_bytes, count, 1, self.mode);
+        write_property!(&mut config_bytes, count, 5, self.copy_on_read);
+        write_property!(&mut config_bytes, count, 100, self.data_region_name);
+        write_property!(&mut config_bytes, count, 405, self.eager_ttl);
+        write_property!(&mut config_bytes, count, 406, self.statistics_enabled);
+        write_property!(&mut config_bytes, count, 400, self.group_name);
+        write_property!(&mut config_bytes, count, 402, self.default_lock_timeout);
+        write_property!(&mut config_bytes, count, 403, self.max_concurrent_async_operations);
+        write_property!(&mut config_bytes, count, 206, self.max_query_iterators);
+        write_property!(&mut config_bytes, count, 0, self.name);
+        write_property!(&mut config_bytes, count, 101, self.on_heap_cache_enabled);
+        write_property!(&mut config_bytes, count, 404, self.partition_loss_policy);
+        write_property!(&mut config_bytes, count, 202, self.query_detail_metrics_size);
+        write_property!(&mut config_bytes, count, 201, self.query_parallelism);
+        write_property!(&mut config_bytes, count, 6, self.read_from_backup);
+        write_property!(&mut config_bytes, count, 303, self.rebalance_batch_size);
+        write_property!(&mut config_bytes, count, 304, self.rebalance_batch_prefetch_count);
+        write_property!(&mut config_bytes, count, 301, self.rebalance_delay);
+        write_property!(&mut config_bytes, count, 300, self.rebalance_mode);
+        write_property!(&mut config_bytes, count, 305, self.rebalance_order);
+        write_property!(&mut config_bytes, count, 306, self.rebalance_throttle);
+        write_property!(&mut config_bytes, count, 302, self.rebalance_timeout);
+        write_property!(&mut config_bytes, count, 205, self.sql_escape_all);
+        write_property!(&mut config_bytes, count, 204, self.sql_index_inline_max_size);
+        write_property!(&mut config_bytes, count, 203, self.sql_schema);
+        write_property!(&mut config_bytes, count, 4, self.write_synchronization_mode);
+        write_property!(&mut config_bytes, count, 401, self.cache_key_configurations);
+        write_property!(&mut config_bytes, count, 200, self.query_entities);
 
-        bytes.put_i32_le(config_bytes.len() as i32);
-        bytes.put_i16_le(30);
+        bytes.put_i32_le(2 + config_bytes.len() as i32);
+        bytes.put_i16_le(count);
         bytes.put(config_bytes);
 
         Ok(())
     }
-}
-
-fn write_property<T: IgniteWrite>(bytes: &mut BytesMut, code: i16, value: &T) -> Result<()> {
-    bytes.put_i16(code);
-
-    value.write(bytes)
 }
